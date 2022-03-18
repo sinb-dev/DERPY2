1f37bd92ec17f6c328d17a860c4af15005cf6473
sinb-dev <sinb@techcollege.dk>
2022-03-18 13:55:10 +0100

    D1: Added company

---
+++ Model/Company.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DERPY.Model
+{
+    public class Company
+    {
+        /*Firmanavn
+• Vej
+• Husnummer
+• Postnummer
+• By
+• Land
+• Valuta
+        */
+        public string CompanyName { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Currency { get; set; }
+    }
+    }
+}
