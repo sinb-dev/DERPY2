22863b1e9407951a43b70b3c231eb8dd60dae3c5
sinb-dev <sinb@techcollege.dk>
2022-03-18 13:50:20 +0100

    Add project files.

---
+++ Data/Database.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DERPY.Data
+{
+    public partial class Database
+    {
+        public static Database Instance { get; private set; }
+        static Database()
+        {
+            Instance = new Database();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DERPY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
