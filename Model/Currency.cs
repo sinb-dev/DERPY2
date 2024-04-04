23388a5b8e15bc0391527bece7ee94a6df7fb7af
sinb-dev <sinb@techcollege.dk>
2024-04-04 10:41:58 +0200

    D1 - Added currency

---
+++ Model/Currency.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DERPY.Model
+{
+    public enum Currency
+    {
+        DKK, SEK, USD, EUR
+    }
+}
+
