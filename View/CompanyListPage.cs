02298e058accad084ac2569b5e8f48c6d35f9340
sinb-dev <sinb@techcollege.dk>
2024-04-05 11:35:14 +0200

    Cleanup added comments

--- View/CompanyListPage.cs
+++ View/CompanyListPage.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Data.SQLite;
+using DERPY.Data;
 using DERPY.Model;
 using TECHCOOL.UI;
 
@@ -7,20 +7,35 @@ namespace DERPY
 {
     public class CompanyListPage : Screen
     {
+        //Set the title of this page
         public override string Title {get;set;} = "Selskaber";
 
         protected override void Draw()
         {
             Console.CursorVisible = false;
+
+            //A ListPage draws columns and rows in a box
             ListPage<Company> listPage = new();
+
+            //Add some columns
             listPage.AddColumn("Selskab", nameof(Company.CompanyName), 40);
             listPage.AddColumn("Land", nameof(Company.Country));
-            listPage.AddColumn("Valuta", nameof(Company.Currency), 5);
+            listPage.AddColumn("Valuta", nameof(Company.Currency), 8);
 
-            listPage.Add( new Company {CompanyName = "Foderbrættet A/S", Country = "Danmark", Currency = Currency.DKK });
-            listPage.Add( new Company {CompanyName = "Foodboard Ltd", Country = "USA", Currency = Currency.USD });
+            //Get companies from the database and add them to the list
+            var companies = Database.Instance.GetCompanies();
+            foreach (Company model in companies) 
+            {
+                listPage.Add(model);
+            }
 
-            listPage.Select();
+            //Enable selection of a company by using arrow keys
+            var company = listPage.Select();
+            if (company != null) 
+            {
+                //Ask Screen class to display the CompanyDetails view
+                Screen.Display(new CompanyDetails(company));
+            }
         }
     }
 }
