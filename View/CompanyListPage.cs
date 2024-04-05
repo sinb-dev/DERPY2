3b85db6702cc3385337513a53a8d87ec72100d14
sinb-dev <sinb@techcollege.dk>
2024-04-05 11:36:06 +0200

    V1 Removed namespace

--- View/CompanyListPage.cs
+++ View/CompanyListPage.cs
@@ -3,39 +3,37 @@ using DERPY.Data;
 using DERPY.Model;
 using TECHCOOL.UI;
 
-namespace DERPY
+namespace DERPY;
+public class CompanyListPage : Screen
 {
-    public class CompanyListPage : Screen
+    //Set the title of this page
+    public override string Title {get;set;} = "Selskaber";
+
+    protected override void Draw()
     {
-        //Set the title of this page
-        public override string Title {get;set;} = "Selskaber";
+        Console.CursorVisible = false;
 
-        protected override void Draw()
+        //A ListPage draws columns and rows in a box
+        ListPage<Company> listPage = new();
+
+        //Add some columns
+        listPage.AddColumn("Selskab", nameof(Company.CompanyName), 40);
+        listPage.AddColumn("Land", nameof(Company.Country));
+        listPage.AddColumn("Valuta", nameof(Company.Currency), 8);
+
+        //Get companies from the database and add them to the list
+        var companies = Database.Instance.GetCompanies();
+        foreach (Company model in companies) 
         {
-            Console.CursorVisible = false;
+            listPage.Add(model);
+        }
 
-            //A ListPage draws columns and rows in a box
-            ListPage<Company> listPage = new();
-
-            //Add some columns
-            listPage.AddColumn("Selskab", nameof(Company.CompanyName), 40);
-            listPage.AddColumn("Land", nameof(Company.Country));
-            listPage.AddColumn("Valuta", nameof(Company.Currency), 8);
-
-            //Get companies from the database and add them to the list
-            var companies = Database.Instance.GetCompanies();
-            foreach (Company model in companies) 
-            {
-                listPage.Add(model);
-            }
-
-            //Enable selection of a company by using arrow keys
-            var company = listPage.Select();
-            if (company != null) 
-            {
-                //Ask Screen class to display the CompanyDetails view
-                Screen.Display(new CompanyDetails(company));
-            }
+        //Enable selection of a company by using arrow keys
+        var company = listPage.Select();
+        if (company != null) 
+        {
+            //Ask Screen class to display the CompanyDetails view
+            Screen.Display(new CompanyDetails(company));
         }
     }
 }
